d075f3323f81d1d13361aa940a86f9fb39d43b9d
alonez12 <35674047@qq.com>
2019-05-28 10:53:24 +0800

    add JFrame

--- src/chess.java
+++ src/chess.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
 public class chess {
     public static void main(String[] args){
+        chess frame = new chess();
         System.out.println("Hello World!");
+    }
+    public chess(){
+        JFrame newframe = new JFrame("first");
+        newframe.setSize(640,480);
+        newframe.setVisible(true);
     }
 }
